de06a5d1d9aed947e69e42856cb19dfd1f3dd1dd
Mathieu Bastian <mathieu.bastian@gmail.com>
2022-01-02 13:06:06 +0100

    Improve build script

--- specs/build.sh
+++ specs/build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 Help()
 {
@@ -17,20 +17,10 @@ then
    exit
 fi
 
-echo "Rebuilding all XSD files in $dir"
+echo "Rebuilding all files in $dir"
 
-trang -I rnc -O xsd $dir/data.rnc $dir/data.xsd
-trang -I rnc -O xsd $dir/dynamics.rnc $dir/dynamics.xsd
-trang -I rnc -O xsd $dir/hierarchy.rnc $dir/hierarchy.xsd
-trang -I rnc -O xsd $dir/phylogenics.rnc $dir/phylogenics.xsd
-trang -I rnc -O xsd $dir/viz.rnc $dir/viz.xsd
-trang -I rnc -O xsd $dir/gexf.rnc $dir/gexf.xsd
-
-echo "Rebuilding all RNG files in $dir"
-
-trang -I rnc -O rng $dir/data.rnc $dir/data.rng
-trang -I rnc -O rng $dir/dynamics.rnc $dir/dynamics.rng
-trang -I rnc -O rng $dir/hierarchy.rnc $dir/hierarchy.rng
-trang -I rnc -O rng $dir/phylogenics.rnc $dir/phylogenics.rng
-trang -I rnc -O rng $dir/viz.rnc $dir/viz.rng
-trang -I rnc -O rng $dir/gexf.rnc $dir/gexf.rng
+while IFS= read -r -d $'\0' rncfile; do
+  echo "Building file $(basename "${rncfile}")"
+  trang -I rnc -O xsd $rncfile $dir/$(basename "${rncfile}" .rnc).xsd
+  trang -I rnc -O rng $rncfile $dir/$(basename "${rncfile}" .rnc).rng
+done < <(find "$dir" -name "*.rnc" -print0)
